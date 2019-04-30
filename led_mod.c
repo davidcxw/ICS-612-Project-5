405fb63fa065c9eb82d2917603e0da7d1860eefd
omorim <omorim@hawaii.edu>
2019-04-29 18:03:15 -1000

    cpu input

--- led_mod.c
+++ led_mod.c
@@ -16,57 +16,80 @@
 #define L8 22
 
 MODULE_LICENSE("GPL");
+int num_leds;
+int i;
+int leds[10];
 
 static int __init pix_init(void){
+  leds[1] = 18;
+  leds[2] = 23;
+  leds[3] = 27;
+  leds[4] = 16;
+  leds[5] = 17;
+  leds[6] = 26;
+  leds[7] = 13;
+  leds[8] = 22;
+  
   printk(KERN_INFO "Initialized");
 
-  gpio_request(L1, "L1");
-  gpio_direction_output(L1, true);
-  gpio_set_value(L1, true);
+  gpio_request(leds[1], "L1");
+  gpio_direction_output(leds[1], true);
+  //gpio_set_value(leds[1], true);
 
-  gpio_request(L2, "L2");
-  gpio_direction_output(L2, true);
-  gpio_set_value(L2, true);
+  gpio_request(leds[2], "L2");
+  gpio_direction_output(leds[2], true);
+  //gpio_set_value(leds[2], true);
 
-  gpio_request(L3, "L3");
-  gpio_direction_output(L3, true);
-  gpio_set_value(L3, true);
+  gpio_request(leds[3], "L3");
+  gpio_direction_output(leds[3], true);
+  //gpio_set_value(leds[3], true);
 
-  gpio_request(L4, "L4");
-  gpio_direction_output(L4, true);
-  gpio_set_value(L4, true);
+  gpio_request(leds[4], "L4");
+  gpio_direction_output(leds[4], true);
+  //gpio_set_value(leds[4], true);
 
-  gpio_request(L5, "L5");
-  gpio_direction_output(L5, true);
-  gpio_set_value(L5, true);
+  gpio_request(leds[5], "L5");
+  gpio_direction_output(leds[5], true);
+  //gpio_set_value(leds[5], true);
 
-  gpio_request(L6, "L6");
-  gpio_direction_output(L6, true);
-  gpio_set_value(L6, true);
+  gpio_request(leds[6], "L6");
+  gpio_direction_output(leds[6], true);
+  //gpio_set_value(leds[6], true);
 
-  gpio_request(L7, "L7");
-  gpio_direction_output(L7, true);
-  gpio_set_value(L7, true);
+  gpio_request(leds[7], "L7");
+  gpio_direction_output(leds[7], true);
+  //gpio_set_value(leds[7], true);
 
-  gpio_request(L8, "L8");
-  gpio_direction_output(L8, true);
-  gpio_set_value(L8, true);
+  gpio_request(leds[8], "L8");
+  gpio_direction_output(leds[8], true);
+  //gpio_set_value(leds[8], true);
+  
+  //gpio_request(leds[9], "L9");
+  //gpio_direction_output(leds[9], true);
 
   printk(KERN_INFO "Finished Initalization");
+  num_leds = 8;
+  for(i = 1; i < num_leds + 1; i++){
+    gpio_set_value(leds[i], true);
+  }
+  for(i = num_leds + 1; i < 10; i ++){
+    gpio_set_value(leds[i], false);
+  }
   return 0;
 }
 
 static void __exit pix_exit(void){
   printk(KERN_INFO "Removing module");
 
-  gpio_free(L1);
-  gpio_free(L2);
-  gpio_free(L3);
-  gpio_free(L4);
-  gpio_free(L5);
-  gpio_free(L6);
-  gpio_free(L7);
-  gpio_free(L8);
+  gpio_free(leds[1]);
+  gpio_free(leds[2]);
+  gpio_free(leds[3]);
+  gpio_free(leds[4]);
+  gpio_free(leds[5]);
+  gpio_free(leds[6]);
+  gpio_free(leds[7]);
+  gpio_free(leds[8]);
+  //gpio_free(leds[9]);
 
   printk(KERN_INFO "Finished removing");
 }
